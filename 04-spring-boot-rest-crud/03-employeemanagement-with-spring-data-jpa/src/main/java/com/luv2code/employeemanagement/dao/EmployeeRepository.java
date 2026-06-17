6534e36c37fe264aa5849dcbc60a985565402b89
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2026-06-17 08:32:17 +0100

    Introduced spring data jpa

---
+++ 04-spring-boot-rest-crud/03-employeemanagement-with-spring-data-jpa/src/main/java/com/luv2code/employeemanagement/dao/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.luv2code.employeemanagement.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<EmployeeRepository, Integer> {
+
+}
