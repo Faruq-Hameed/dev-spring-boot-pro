4ea673eb9e72310c916e54763583bb2567212eaf
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2026-06-20 08:04:46 +0100

    Created employee manager with spring data jpa

---
+++ 04-spring-boot-rest-crud/03-employeemanagement-with-spring-data-rest/src/test/java/com/luv2code/employeemanagement/EmployeemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
