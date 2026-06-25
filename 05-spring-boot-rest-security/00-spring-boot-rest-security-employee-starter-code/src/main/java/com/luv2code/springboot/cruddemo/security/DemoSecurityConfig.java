e56b97c27ccfe9de769067baa1cb43abee8630fb
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2026-06-25 15:37:48 +0100

    added security flter

---
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,66 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+    @Bean
+    /* This is for UserDetails Managements i.e permitted users to sign in and make request**/
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}pass")
+                .roles("EMPLOYEE")
+                .build();
+        System.out.println(john.getAuthorities());
+
+        UserDetails mary = User.builder() //Building a user so no longer default
+                .username("mary") //the username should be faruq
+                .password("{noop}pass1") //the password is pass plainly i.e noop
+                .roles("EMPLOYEE", "MANAGER")
+                .build(); //now build this user.
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}pass")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        UserDetails faruq = User.builder()
+                .username("faruq")
+                .password("{noop}pass")
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN")//no role
+                .build();
+        return new InMemoryUserDetailsManager(john, mary, susan, faruq); //return the instance of InMemo...
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        //configure the every request must be authenticated
+        httpSecurity.authorizeHttpRequests(configurer ->{
+
+           configurer
+                   .requestMatchers(HttpMethod.GET, "/api").anonymous()
+                   .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE") //anyone can access this
+                   .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE") //must have employee role to access
+                   .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                   .requestMatchers(HttpMethod.PUT, "/api/employee/**").hasRole("ADMIN")
+                   .requestMatchers(HttpMethod.PATCH, "/api/employee/**").hasRole("MANAGER")
+                   .requestMatchers(HttpMethod.PUT, "/api/employee/**").hasRole("ADMIN")
+                   .anyRequest().authenticated();
+        });
+
+        httpSecurity.httpBasic(Customizer.withDefaults()); //use basic auth
+        httpSecurity.csrf(crsf -> crsf.disable());// disable crsf
+        return httpSecurity.build();
+    }
+
+}
