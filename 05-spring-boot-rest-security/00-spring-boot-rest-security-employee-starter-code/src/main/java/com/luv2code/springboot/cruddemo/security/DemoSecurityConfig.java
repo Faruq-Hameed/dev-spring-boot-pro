eda6f1294cb46e36d5f5c8db15ea33cf1f225296
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2026-07-16 12:34:38 +0100

    fixed column quert err

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -27,7 +27,7 @@ public class DemoSecurityConfig {
                 "select user_id, pw, active from members where user_id=?"
         );
         //define query to retrieve
-        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, roles from roles where user_id=?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
         return jdbcUserDetailsManager;
     }
 
