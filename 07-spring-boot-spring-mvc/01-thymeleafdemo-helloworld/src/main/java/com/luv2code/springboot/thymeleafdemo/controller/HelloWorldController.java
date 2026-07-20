3ee2460bb5b75aadb5e4a1fd9d6fd25fe33afd94
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2026-07-20 08:36:18 +0100

    added and updated the endpoints

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class HelloWorldController {
     //controller method to show the form
-    @GetMapping("/hello")
+    @GetMapping("/form")
     public String showForm(){
         return "helloworld-form";
     }
 
     //controller method to show d form
     @PostMapping("/hello")
+    public String createHello(Model model){
+
+    }
 }
