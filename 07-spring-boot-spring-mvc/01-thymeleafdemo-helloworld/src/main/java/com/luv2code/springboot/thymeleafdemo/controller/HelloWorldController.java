efb6bc6441482dc824996da35ff8eb118c09d70d
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2026-07-24 08:10:39 +0100

    added and updated the endpoints

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -22,7 +22,7 @@ public class HelloWorldController {
     }
 
     @PostMapping("/process")
-    public String processForm(HttpServletRequest request, Model model){
+//    public String processForm(HttpServletRequest request, Model model){
 //    public String processForm(@RequestParam String studentName, Model model){
         //read the request parameter from the HTML form
         String studentName = request.getParameter("studentName");
