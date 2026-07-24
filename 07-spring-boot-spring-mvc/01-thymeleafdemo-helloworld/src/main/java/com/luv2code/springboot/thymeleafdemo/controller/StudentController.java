7de7e8bc6afd306957c2de41fb3fb162486f9164
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2026-07-24 09:18:07 +0100

    added Student functs

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,8 +1,13 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudentController {
 
+    @GetMapping("signin")
+    public String getSignInForm(){
+        return "student-form";
+    }
 }
