8d77b6a9d42b9b7744a999b943b7edc75e1e9fc0
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2026-08-01 20:18:32 +0100

    added Student functs

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -18,7 +18,7 @@ public class Student {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
+    public void setFirstName(String firstName)
         this.firstName = firstName;
     }
 
